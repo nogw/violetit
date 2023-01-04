098900e5097accc4fb9838b60bcea26820927a0a
nogw <gabrielnogueiraoliveira@gmail.com>
2023-01-04 02:10:37 -0300

    feat(web): vite resolve imports with '@'

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        app: './public/index.html',
-      },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
