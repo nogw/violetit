e9899a3ee4ba3de9468877735bd05bfc53c2e8df
nogw <gabrielnogueiraoliveira@gmail.com>
2023-01-04 02:25:17 -0300

    feat(ui): add error message styling

--- packages/ui/src/ErrorText.tsx
+++ packages/ui/src/ErrorText.tsx
@@ -1,7 +1,19 @@
+import clsx from 'clsx';
+
 interface ErrorTextProps {
   children: React.ReactNode;
 }
 
-export const ErrorText = ({ children }: ErrorTextProps) => {
-  return <h1>{children}</h1>;
-};
+export const ErrorText = ({ children }: ErrorTextProps) => (
+  <div
+    className={clsx(
+      'bg-red-100 border-l-4 border-red-500',
+      'rounded mt-4 px-3 py-1',
+      'text-red-700',
+    )}
+    role="alert"
+  >
+    <p className="font-bold">Error</p>
+    <p>{children}</p>
+  </div>
+);
