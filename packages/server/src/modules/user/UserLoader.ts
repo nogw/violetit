5a45b75177f06780249e38fd1c36f3ab80628d12
nogw <gabrielnogueiraoliveira@gmail.com>
2023-01-06 03:16:33 -0300

    feat(server): add UserLoader

---
+++ packages/server/src/modules/user/UserLoader.ts
@@ -0,0 +1,15 @@
+import { createLoader } from '@entria/graphql-mongo-helpers';
+
+import { registerLoader } from '../loader/loaderRegister';
+import { UserModel } from './UserModel';
+
+const Loader = createLoader({
+  model: UserModel,
+  loaderName: 'UserLoader',
+  shouldValidateContextUser: true,
+});
+
+export const { Wrapper: User, getLoader, clearCache, load, loadAll } = Loader;
+export default Loader;
+
+registerLoader('UserLoader', getLoader);
