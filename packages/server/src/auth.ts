da1ae70cdbfdd2eeede0a47d4c7f9f3e3269261a
nogw <gabrielnogueiraoliveira@gmail.com>
2023-01-07 02:36:22 -0300

    feat(server): create auth functions to work with jwt

---
+++ packages/server/src/auth.ts
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+import { Maybe } from '@violetit/types';
+
+import { UserModel, IUserDocument } from './modules/user/UserModel';
+import { config } from './config';
+
+export const getUser = async (
+  token: Maybe<string>,
+): Promise<Maybe<IUserDocument>> => {
+  if (!token) return null;
+
+  const subToken = token.substring(4);
+  const decodedToken = jwt.verify(subToken, config.MONGO_URI) as { id: string };
+
+  const user = await UserModel.findOne({ _id: decodedToken.id });
+
+  if (!user) return null;
+
+  return user;
+};
+
+export const generateJwtToken = (userId: string) => {
+  return `JWT ${jwt.sign({ id: userId }, config.JWT_SECRET)}`;
+};
