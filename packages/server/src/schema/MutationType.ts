e7d5b9aaf6c783880f0e422bf9ddbd2bb818e41d
nogw <gabrielnogueiraoliveira@gmail.com>
2023-01-12 01:32:04 -0300

    feat(server): add mutations to mutation root

--- packages/server/src/schema/MutationType.ts
+++ packages/server/src/schema/MutationType.ts
@@ -1,11 +1,17 @@
 import { GraphQLObjectType } from 'graphql';
 
 import * as userMutations from '../modules/user/mutations';
+import * as communityMutations from '../modules/community/mutations';
+import * as postMutations from '../modules/post/mutations';
+import * as voteMutations from '../modules/vote/mutations';
 
 export const MutationType = new GraphQLObjectType({
   name: 'MutationType',
   description: 'Root of all mutations',
   fields: () => ({
     ...userMutations,
+    ...communityMutations,
+    ...postMutations,
+    ...voteMutations,
   }),
 });
