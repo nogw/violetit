1896a8e7ffcbb03c5028117d2795aebd9621d92a
nogw <gabrielnogueiraoliveira@gmail.com>
2023-01-12 02:26:08 -0300

    feat(web): create vote module

---
+++ packages/web/src/modules/post/vote/VoteCreateMutation.ts
@@ -0,0 +1,11 @@
+import { graphql } from 'react-relay';
+
+export const VoteCreate = graphql`
+  mutation VoteCreateMutation($input: VoteCreateInput!) {
+    VoteCreate(input: $input) {
+      post {
+        votesCount
+      }
+    }
+  }
+`;
