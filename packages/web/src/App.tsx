e6bc9a5ea7ccbcef8813ef2ded9ff8fb6ee8b11d
nogw <gabrielnogueiraoliveira@gmail.com>
2023-01-15 02:12:11 -0300

    feat(web): add Loading component to react.suspend

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import { Providers } from './Providers';
-
 import { Loading } from '@violetit/ui';
+
+import { Providers } from './Providers';
+import { Routes } from './Routes';
 
 import './styles/index.css';
 
 const App = () => (
   <Providers>
-    <React.Suspense fallback={'Loading...'}>
-      <Loading />
+    <React.Suspense fallback={<Loading />}>
+      <Routes />
     </React.Suspense>
   </Providers>
 );
