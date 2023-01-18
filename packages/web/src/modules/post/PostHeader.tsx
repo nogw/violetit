21019f1b1a5fe9aaa8d28f042948423656444611
nogw <gabrielnogueiraoliveira@gmail.com>
2023-01-18 02:26:27 -0300

    feat(web): use graphql fragment to fetch PostHeader data

--- packages/web/src/modules/post/PostHeader.tsx
+++ packages/web/src/modules/post/PostHeader.tsx
@@ -2,26 +2,44 @@ import { Flex } from '@violetit/ui';
 
 import { timeAgo } from '@/lib/timeAgo';
 import { Link } from '@/shared-components/Link';
+import { graphql, useFragment } from 'react-relay';
+import { PostHeader_post$key } from './__generated__/PostHeader_post.graphql';
 
 type PostHeaderProps = {
-  community: string;
-  author: string;
-  createdAt: Date;
+  post: PostHeader_post$key;
 };
 
-const PostHeader = ({ community, author, createdAt }: PostHeaderProps) => {
+const PostHeader = (props: PostHeaderProps) => {
+  const post = useFragment<PostHeader_post$key>(
+    graphql`
+      fragment PostHeader_post on Post {
+        id
+        author {
+          username
+        }
+        community {
+          name
+        }
+        createdAt
+      }
+    `,
+    props.post,
+  );
+
+  const { author, community } = post;
+
   return (
     <Flex className="text-xs text-gray-500 gap-2">
-      <Link to={`/u/${author}`}>
-        <h3 className="text-black font-medium">{`r/${community}`}</h3>
+      <Link to={`/u/${community?.name}`}>
+        <h3 className="text-black font-medium">{`r/${community?.name}`}</h3>
       </Link>
       <p className="text-gray-400">•</p>
       <Flex className="gap-1">
         <p>Posted by</p>
-        <Link to={`/u/${author}`}>
-          <p className="mr-1 text-gray-500 font-normal">{`u/${author}`}</p>
+        <Link to={`/u/${author?.username}`}>
+          <p className="mr-1 text-gray-500 font-normal">{`u/${author?.username}`}</p>
         </Link>
-        <p>{`${timeAgo(createdAt)} ago`}</p>
+        <p>{`${timeAgo(new Date(post.createdAt || ''))} ago`}</p>
       </Flex>
     </Flex>
   );
