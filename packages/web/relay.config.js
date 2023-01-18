56eebd92eb1d6d73ba6c63da6c7cfe4bf93af08f
nogw <gabrielnogueiraoliveira@gmail.com>
2023-01-18 02:31:10 -0300

    feat(web): add modules to exclude in relay config

--- packages/web/relay.config.js
+++ packages/web/relay.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  language: 'typescript',
   src: './src',
+  language: 'typescript',
   schema: '../server/graphql/schema.graphql',
+  eagerEsModules: true,
+  exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
 };
---
+++ packages/web/src/modules/post/PostPage.tsx
@@ -0,0 +1,7 @@
+// import { PostDetail } from './PostDetail';
+
+const PostDetailPage = () => {
+  return <div />;
+};
+
+export default PostDetailPage;
