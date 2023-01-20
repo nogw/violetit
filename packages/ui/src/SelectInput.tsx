587cad81bb21e61e3c9fdc35bf5c05d5479620b1
nogw <gabrielnogueiraoliveira@gmail.com>
2023-01-20 01:56:02 -0300

    feat(ui): create SelectInput ui component

---
+++ packages/ui/src/SelectInput.tsx
@@ -0,0 +1,30 @@
+import clsx from 'clsx';
+
+type SelectInputProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
+  initial: string;
+  options: string[];
+};
+
+export const SelectInput = ({
+  className,
+  initial,
+  options,
+}: SelectInputProps) => {
+  return (
+    <select
+      className={clsx(
+        'rounded-lg border border-gray-300',
+        'text-gray-900 text-sm',
+        'w-full outline-none cursor-pointer bg-white p-2',
+        className,
+      )}
+    >
+      <option defaultValue={initial}>{initial}</option>
+      {options.map((option, index) => (
+        <option key={index} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+};
