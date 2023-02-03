6b29c4f455f552a41eadb7aadcddb8562a7c3914
nogw <gabrielnogueiraoliveira@gmail.com>
2023-02-03 02:13:57 -0300

    refactor(web): rename props

--- packages/web/src/modules/post/PostComposer.tsx
+++ packages/web/src/modules/post/PostComposer.tsx
@@ -3,14 +3,14 @@ import { Card, Input } from '@violetit/ui';
 import { Link } from '@/shared-components/Link';
 
 type PostComposerProps = {
-  communityName?: string;
+  community?: string;
 };
 
-export const PostComposer = ({ communityName }: PostComposerProps) => {
-  const link = communityName ? `/submit/${communityName}` : '/submit';
+export const PostComposer = ({ community }: PostComposerProps) => {
+  const link = community ? `/submit/${community}` : '/submit';
 
   return (
-    <Card className="p-2">
+    <Card className="p-2 mb-0">
       <Link className="w-full" to={link}>
         <Input placeholder="Create post" />
       </Link>
