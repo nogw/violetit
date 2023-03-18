8c6b109eaffb15e4640a229c6613fd162fc9ede4
nogw <gabrielnogueiraoliveira@gmail.com>
2023-03-18 00:51:59 -0300

    feat: add tailwind extension

--- packages/web/tailwind.config.cjs
+++ packages/web/tailwind.config.cjs
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    '../ui/src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', '../ui/src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/typography')],
 };
