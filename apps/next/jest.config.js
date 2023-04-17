0ec7010d6caacd9164007b2129021c4bae3d5aa6
nogw <gabrielnogueiraoliveira@gmail.com>
2023-04-17 12:32:53 -0300

    feat: jest config

--- apps/next/jest.config.js
+++ apps/next/jest.config.js
@@ -1,10 +1,22 @@
 const nextJest = require('next/jest');
 
-const createJestConfig = nextJest({ dir: process.cwd() });
+const createJestConfig = nextJest({
+  dir: './',
+});
 
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules', 'src'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: ['node_modules.+\\.(js|jsx)$'],
+  coverageThreshold: {
+    global: {
+      branches: 30,
+      functions: 30,
+      lines: 30,
+      statements: 30,
+    },
+  },
 };
 
-module.exports = createJestConfig(customJestConfig);
+module.exports = await createJestConfig(customJestConfig)();
